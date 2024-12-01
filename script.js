d9db09ea557b981f9ae4c7901335fbec4f6e2efd
nagyszabolcsb <sznabi10@gmail.com>
2024-12-01 16:13:54 +0100

    automatikus galeria slide

--- script.js
+++ script.js
@@ -16,36 +16,39 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-let slideIndex = 1;
+let slideIndex = 0;
 
-function showSlides(n) {
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
 
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
 
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  
-  for (let i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+
+    setTimeout(showSlides, 3000);
 }
 
-// Next/previous controls
+showSlides();
+
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    slideIndex += n - 1;
+    showSlides();
 }
 
-// Current slide
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    slideIndex = n - 1;
+    showSlides();
 }
-
-// Initialize the slideshow
-showSlides(slideIndex);
